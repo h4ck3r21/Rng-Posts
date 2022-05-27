80c6e9aa34fa3bbdb285920acba17c4830649bee
h4ck3r21 <nineplustenequals21.a@gmail.com>
2022-05-28 08:02:36 +1000

    I Finished creating the form so that it can display the image and created a button icon to upload the file. I began trying to get it to be able to upload multiple images, by implementing javascript to allow it display the images as slideshow.

--- static/image-render.js
+++ static/image-render.js
@@ -23,3 +23,22 @@ function getContent(){
         document.getElementById("body").value = document.getElementById("body-input").innerHTML;
         console.log(document.getElementById("body").value)
     }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
